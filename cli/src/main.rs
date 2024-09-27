9f157a81dd03922bc6a3245e6025ac91fa0b2681
hellojason3 <119396233+hellojason3@users.noreply.github.com>
2024-09-27 09:16:27 +0800

    fix: compile time too long

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,9 +1,13 @@
 use std::path::PathBuf;
 use ark_r1cs_std::fields::fp::FpVar;
+use log::{info, error, debug, trace, warn};
+use env_logger::Builder;
+use std::env;
+use chrono::Local;
 use swiftness_field::{Fp, SimpleField};
 pub use swiftness_proof_parser::*;
 pub use swiftness_stark::*;
-
+use std::io::Write;
 #[cfg(feature = "dex")]
 use swiftness_air::layout::dex::Layout;
 #[cfg(feature = "recursive")]
@@ -19,6 +23,10 @@ use swiftness_air::layout::starknet_with_keccak::Layout;
 
 use clap::Parser;
 use swiftness_utils::curve::StarkwareCurve;
+use jemallocator::Jemalloc;
+
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -28,7 +36,32 @@ struct CairoVMVerifier {
     proof: PathBuf,
 }
 
+fn init_logger(){
+    env_logger::init();
+    // let mut builder = Builder::from_default_env();
+    // builder.format(|buf, record| {
+    //     let now = Local::now();
+    //     let datetime = now.format("%Y-%m-%d %H:%M:%S");
+    //     writeln!(
+    //         buf,
+    //         "{} [{}] ({}:{}) : {}",
+    //         datetime,
+    //         record.level(),
+    //         record.file().unwrap_or("unknown"),
+    //         record.line().unwrap_or(0),
+    //         record.args()
+    //     )
+    // });
+    //
+    // builder.init();
+    error!("test error");
+    warn!("test warn");
+    info!("test info");
+    debug!("test debug");
+    trace!("test trace");
+}
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    init_logger();
     let cli = CairoVMVerifier::parse();
     let stark_proof = parse(std::fs::read_to_string(cli.proof)?)?;
     let security_bits: FpVar<Fp> = stark_proof.config.security_bits();
